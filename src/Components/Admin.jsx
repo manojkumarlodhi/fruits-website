4c68537d69fa5180ecbd17b7d55b4a7a3f46a6aa
manojlodhi <manojlodho0262@gmail.com>
2024-10-09 11:13:51 +0530

    some changes

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -10,6 +10,8 @@ function Admin() {
         <input type="text"  placeholder='Enter Your Name' />
         <input type="password" placeholder='Password' />
         <input type="submit" value="submit" />
+        <input type="text" />
+        
     </form>
       
     </>
