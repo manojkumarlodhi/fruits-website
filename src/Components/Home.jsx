064f3d6126485a5dcf4d9bc318cb388fa493dac4
manojlodhi <manojlodho0262@gmail.com>
2024-10-10 01:58:46 +0530

    add latest fruits

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,27 +7,110 @@ import img5 from '../assets/image/img5.png';
 import img6 from '../assets/image/img6.png';
 import img7 from '../assets/image/img7.png';
 import img8 from '../assets/image/img8.png';
+import waterImg from '../assets/image/Water.webp';
+import longan from '../assets/image/LONGAN.webp';
+import guava from '../assets/image/PinkGuava.webp';
+import rambutan from '../assets/image/Rambutan.webp';
+import { IoEyeSharp } from "react-icons/io5";
+import { FaHeart } from "react-icons/fa";
+import { PiTrolleyFill } from "react-icons/pi";
+
+
 
 
 function Home() {
   return (
-    <div id="carouselExampleAutoplaying" className="carousel slide relative" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        {[img1, img2, img3, img4, img5, img6, img7, img8].map((img, index) => (
-          <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
-            <img src={img} className="d-block w-full h-[80vh] object-cover" alt={`img${index + 1}`} />
-          </div>
-        ))}
+    <>
+      <div id="carouselExampleAutoplaying" className="carousel slide relative" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          {[img1, img2, img3, img4, img5, img6, img7, img8].map((img, index) => (
+            <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+              <img src={img} className="d-block w-full h-[80vh] object-cover" alt={`img${index + 1}`} />
+            </div>
+          ))}
+        </div>
+        <button className="carousel-control-prev rounded-full p-2" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon bg-green-600 rounded-full" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next rounded-full p-2" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon bg-green-600 rounded-full" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
-      <button className="carousel-control-prev rounded-full p-2" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon bg-green-600 rounded-full" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button className="carousel-control-next rounded-full p-2" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-        <span className="carousel-control-next-icon bg-green-600 rounded-full" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+      <div className="text-center text-4xl font-bold p-4 m-4">LATEST FRUIT</div>
+      <div className="flex flex-wrap item-center justify-center w-[90%] gap-24 mx-auto  ">
+        <div className="flex">
+          <div className="">
+            <img src={waterImg} alt="water" className='w-32' />
+          </div>
+          <div>
+            <p>Water Chestnut</p>
+            <p>Green</p>
+            <p>Rs. 600.00</p>
+            <div className='flex gap-3 text-2xl'>
+            <IoEyeSharp className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            <FaHeart className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            <PiTrolleyFill className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            </div>
+          </div>
+
+        </div>
+        <div className="flex">
+          <div className="img">
+            <img src={longan} alt="longan" className='w-32' />
+          </div>
+          <div>
+            <p>Longan -</p>
+            <p>imported</p>
+            <p>Rs. 600.00</p>
+            <div className='flex gap-3 text-2xl'>
+            <IoEyeSharp className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            <FaHeart className='bg-green-500 text-white p-1  rounded-full text-3xl ' />
+            <PiTrolleyFill className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            </div>
+            
+          </div>
+
+
+        </div>
+        <div className="flex">
+          <div className="img">
+            <img src={guava} alt="guava"  className='w-32'/>
+          </div>
+          <div>
+            <p>Pink Guava -</p>
+            <p>Imported</p>
+            <p>Rs. 600.00</p>
+            <div className='flex gap-3 text-2xl'>
+            <IoEyeSharp className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            <FaHeart className='bg-green-500 text-white p-1  rounded-full text-3xl'  />
+            <PiTrolleyFill className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            </div>
+          </div>
+
+        </div>
+        <div className="flex ">
+          <div className="img">
+            <img src={rambutan} alt="rambutan"  className='w-32'/>
+          </div>
+          <div>
+            <p>Rambutan -</p>
+            <p>Imported</p>
+            <p>Rs.600.00</p>
+            <div className='flex gap-2 text-2xl'>
+            <IoEyeSharp className='bg-green-500 text-white p-1  rounded-full text-3xl' />
+            <FaHeart  className='bg-green-500 text-white p-1 rounded-full text-3xl' />
+            <PiTrolleyFill className='bg-green-500 text-white p-1 rounded-full text-3xl' />
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+    </>
+
   );
 }
 
